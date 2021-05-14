d40b9b75586fda96d0433159199d30d2ee104d01
R@MS <sramsiks@gmail.com>
2021-05-14 21:07:38 +0300

    fix in cronjob  and add  callback

--- booking/app/Console/Commands/WatchBookingStatus.php
+++ booking/app/Console/Commands/WatchBookingStatus.php
@@ -7,6 +7,7 @@ use Illuminate\Console\Command;
 use App\Models\CreatedBookings;
 use App\Helpers\JsonRpcClient;
 use Illuminate\Support\Facades\Log;
+use Mailgun\Mailgun;
 
 class WatchBookingStatus extends Command
 {
@@ -72,8 +73,112 @@ class WatchBookingStatus extends Command
                 'X-User-Token: ' . $tokenAdmin
             )
         ));
+/*
+        $sign = md5("7333d9b97d502e002b6ccae5bf8e0b489ef" . env('API_SECRET'));
+        $bookingDetails = $client->getBookingDetails(733, $sign);
+
+        $client = $bookingDetails->client_name;
+        $client_email = $bookingDetails->client_email;
+        $service = $bookingDetails->event_name;
+        $start_date_time = strtotime($bookingDetails->start_date_time);
+
+        $date_start = date('Y-m-d', $start_date_time);
+        $time_start = date('H:i:s', $start_date_time);
+        $addres1 = "";
+        $addres2 = "";
+        foreach ($bookingDetails->additional_fields as $field) {
+            if ($field->field_title === 'Address Line 1') {
+                $addres1 = $field->value;
+            } else if ($field->field_title === 'Address Line 2') {
+                $addres2 = $field->value;
+            }
 
 
+        }
+
+        $clientId=$bookingDetails->client_id;
+
+        $clentDetails = $clientAdmin->getClientList ();
+        $hashes=[];
+        $sign=[];
+foreach($clentDetails as $c){
+    $clientId=$c->id;
+
+    $cd = $clientAdmin->getClientInfo($clientId);
+    $hashes[]=$cd->client_hash;
+    $sign[]= md5($clientId . $cd->client_hash . env('API_SECRET'));
+}
+$t=2;
+
+        $t = 1;
+        $sign = md5($clientId . $clentDetails->client_hash .  env('API_SECRET'));
+
+
+
+        $html = '<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Confirm</title>
+  </head>
+
+  <body>
+<div style="width: 80%;margin: auto;border: 1px solid gray;padding: 33px;color: gray;">
+<h1 style="text-align: center;color: #9d9b9b;"> Appointment scheduled </h1>
+<h4 style="text-align: center;color: black;" > for [client] </h4><hr><table style="width: 100%;">
+<tr>
+<td style="width: 88px;" >What:</td>
+<td style="color: black;" >[service]</td>
+</tr>
+<tr>
+<td style="vertical-align: baseline;" >When:</td>
+<td style="color: black;" >[date_start] [time_start] <br>
+[date_list] </tr>
+<td style="width: 88px;" >Where:</td>
+<td style="color: black;" >[data_field_6]  [data_field_13]</td>
+</tr>
+
+</table>
+<p>Thanks for booking</p>
+<p >Looking forward to see you soon <8</p>
+<p style="text-align: center;"> <a href="[client_bookings_link]"><button style="background-color: #9c00f7; color: white; padding: 15px;  font-size: 18px; font-family: Arial;"> RESCHEDULE/EDIT </button></a></p>
+</div>
+  </body>
+</html>';
+
+        $html = str_replace('[client]', $client, $html);
+        $html = str_replace('[service]', $service, $html);
+        $html = str_replace('[date_start]', $date_start, $html);
+        $html = str_replace('[time_start]', $time_start, $html);
+        $html = str_replace('[time_start]', $time_start, $html);
+        $html = str_replace('[data_field_6]', $addres1, $html);
+        $html = str_replace('[data_field_13]', $addres2, $html);
+
+
+        $mg = Mailgun::create('key-7bf2f2e9b53986eb5d3e028b1ba96f00', 'https://api.eu.mailgun.net'); // For EU servers
+        $mg->messages()->send('mg.miner-stats.com', [
+            'from' => 'bob@example.com',
+            'to' => $client_email,
+            'subject' => 'Test from sdk',
+            'html' => $html
+        ]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        exit();
+        */
         $services = $client->getEventList();
 
         $clients = $clientAdmin->getClientList("", null);
@@ -236,6 +341,7 @@ class WatchBookingStatus extends Command
                                     'require_payment' => $bookingsInfo->bookings[0]->require_payment,
                                     'code' => $bookingsInfo->bookings[0]->code,
                                     'hash' => $bookingsInfo->bookings[0]->hash,
+                                    'status'=>'active'
                                 ]);
 
                             }
--- booking/app/Http/Controllers/CallbackController.php
+++ booking/app/Http/Controllers/CallbackController.php
@@ -7,6 +7,7 @@ use App\Models\BatchBooking;
 use App\Models\CreatedBookings;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Mailgun\Mailgun;
 
 class CallbackController extends Controller
 {
@@ -94,8 +95,9 @@ class CallbackController extends Controller
                     $bookingDetails = $client->getBookingDetails($request->booking_id, $sign);
 
                     $client = $bookingDetails->client_name;
+                    $client_email = $bookingDetails->client_email;
                     $service = $bookingDetails->event_name;
-                    $start_date_time = date_parse($bookingDetails->start_date_time);
+                    $start_date_time = strtotime($bookingDetails->start_date_time);
 
                     $date_start = date('Y-m-d', $start_date_time);
                     $time_start = date('H:i:s', $start_date_time);
@@ -112,7 +114,58 @@ class CallbackController extends Controller
                     }
 
                     $t = 1;
-             //       exit();
+                    //       exit();
+                    $html = '<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Confirm</title>
+  </head>
+
+  <body>
+<div style="width: 80%;margin: auto;border: 1px solid gray;padding: 33px;color: gray;">
+<h1 style="text-align: center;color: #9d9b9b;"> Appointment scheduled </h1>
+<h4 style="text-align: center;color: black;" > for [client] </h4><hr><table style="width: 100%;">
+<tr>
+<td style="width: 88px;" >What:</td>
+<td style="color: black;" >[service]</td>
+</tr>
+<tr>
+<td style="vertical-align: baseline;" >When:</td>
+<td style="color: black;" >[date_start] [time_start] <br>
+[date_list] </tr>
+<td style="width: 88px;" >Where:</td>
+<td style="color: black;" >[data_field_6]  [data_field_13]</td>
+</tr>
+
+</table>
+<p>Thanks for booking</p>
+<p >Looking forward to see you soon <8</p>
+<p style="text-align: center;"> <a href="[client_bookings_link]"><button style="background-color: #9c00f7; color: white; padding: 15px;  font-size: 18px; font-family: Arial;"> RESCHEDULE/EDIT </button></a></p>
+</div>
+  </body>
+</html>';
+
+                    $html = str_replace('[client]', $client, $html);
+                    $html = str_replace('[service]', $service, $html);
+                    $html = str_replace('[date_start]', $date_start, $html);
+                    $html = str_replace('[time_start]', $time_start, $html);
+                    $html = str_replace('[time_start]', $time_start, $html);
+                    $html = str_replace('[data_field_6]', $addres1, $html);
+                    $html = str_replace('[data_field_13]', $addres2, $html);
+
+
+                    $mg = Mailgun::create('key-7bf2f2e9b53986eb5d3e028b1ba96f00', 'https://api.eu.mailgun.net'); // For EU servers
+                    $mg->messages()->send('mg.miner-stats.com', [
+                        'from' => 'bob@example.com',
+                        'to' => $client_email,
+                        'subject' => 'Test from sdk',
+                        'html' => $html
+                    ]);
+
+
                     break;
 
                 case "notify":
